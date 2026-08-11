775b749bdb1e21e4654fc75fa1e0449d10debd3b
Théo Killian <theo.killian5700@gmail.com>
2026-08-11 21:30:56 +0200

    feat:0ui

--- apps/api/src/games/games.service.ts
+++ apps/api/src/games/games.service.ts
@@ -7,7 +7,7 @@ export class GamesService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll() {
-    const games = await this.prisma.game.findMany({
+    const games = await this.prisma.game.findManyOrThrow({
       where: { isActive: true },
       orderBy: { name: 'asc' },
     });
