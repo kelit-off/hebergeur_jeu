673edcb99758e1c2434f14cf25b7ffbc78965e5a
Théo Killian <theo.killian5700@gmail.com>
2026-08-13 21:59:49 +0200

    feat:debutt strcture auth

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,7 +1,28 @@
 export default function LoginPage() {
     return (
         <div>
-            
+            <div>
+                <div>
+                    <p>Connexion</p>
+                </div>
+
+                <form>
+                    <div>
+                        <label>Adresse e-mail</label>
+                        <input id="email" type="email" name="email" placeholder="you@example.com" />
+                    </div>
+
+                    <div>
+                        <label>Mot de passe</label>
+                        <input id="password" type="password" name="password" placeholder="••••••••" />
+                    </div>
+
+                        <button>
+
+                        </button>
+                </form>
+            </div>
+            <p> Tu n'as pas de compte ? <a href="/register">Inscription</a></p>
         </div>
     )
 }
---
+++ apps/web/src/app/(auth)/register/page.tsx
@@ -0,0 +1,26 @@
+export default function RegisterPage() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <p>Inscription</p>
+                </div>
+
+                <form>
+                    <div>
+
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                    <button>
+
+                    </button>
+                </form>
+            </div>
+            <p> Tu as déjà un compte ? <a href="/login">Connexion</a></p>
+        </div>
+    )   
+}
