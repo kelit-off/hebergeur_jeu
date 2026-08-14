6bfbfb1c30fb6050ae45227e716d4f72733c1ec7
Théo Killian <111753429+kelit-off@users.noreply.github.com>
2026-08-14 13:07:07 +0000

    feat:fonctionnement du login

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,4 +1,20 @@
+"use client"
+
+import { useState } from "react"
+
 export default function LoginPage() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+
+    const submit = (e) => {
+        e.preventDefault();
+        
+        
+    }
+
     return (
         <div>
             <div>
@@ -6,19 +22,21 @@ export default function LoginPage() {
                     <p>Connexion</p>
                 </div>
 
-                <form>
-                    <div>
-                        <label>Adresse e-mail</label>
-                        <input id="email" type="email" name="email" placeholder="you@example.com" />
+                <form onSubmit={submit}>
+                    <div className="flex flex-col">
+                        <label htmlFor="email">Adresse e-mail</label>
+                        <div>
+                            <input id="email" type="email" name="email" placeholder="you@example.com" className="rounded-lg py-2 px-4" onChange={(e) => setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value}))} />
+                        </div>
                     </div>
 
                     <div>
                         <label>Mot de passe</label>
-                        <input id="password" type="password" name="password" placeholder="••••••••" />
+                        <input id="password" type="password" name="password" placeholder="••••••••" onChange={(e) => setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }))} />
                     </div>
 
-                        <button>
-
+                        <button type="submit">
+                            Connexion
                         </button>
                 </form>
             </div>
